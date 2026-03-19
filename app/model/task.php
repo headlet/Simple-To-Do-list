a3f6d69605932d5b0db293bd422b3be5bbb770e0
headlet <professionalskill6@gmail.com>
2026-03-19 07:38:16 +0545

    started adding data type in task.php with constructor

--- app/model/task.php
+++ app/model/task.php
@@ -1,5 +1,16 @@
 <?php
 class task {
+    private $id;
+    private $title;
+    private $status;
+
+private function __construct($id, $title, $status='pending')
+{
+    $this->id= $id;
+    $this->title = $title;
+    $this->status = $status;
+}
+
 
 
 }

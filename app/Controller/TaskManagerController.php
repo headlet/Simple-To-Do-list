95f6e2206adebb61e588a60a3a164021dccaedbc
headlet <professionalskill6@gmail.com>
2026-03-19 18:16:52 +0545

    added the tast status icon red for pending and green for complete and tested if the home page shows data from json file using fake data

--- app/Controller/TaskManagerController.php
+++ app/Controller/TaskManagerController.php
@@ -1,31 +1,26 @@
 <?php
 require_once __DIR__ . '/../Models/task.php';
-class TaskManagerController {
+class TaskManagerController
+{
     private $storageFile;
 
     public function __construct()
     {
-        $this->storageFile = require __DIR__ . '/../../storage/task.json';
+        $this->storageFile = __DIR__ . '/../../storage/task.json';
     }
 
-    public function index(){
-        if(file_exists($this->storageFile)){
+    public function index()
+    {
+        if (!file_exists($this->storageFile)) {
             file_put_contents($this->storageFile, json_encode([]));
-            }
-
-        return json_decode(file_get_contents($this->storageFile, true));
+        }
+        $data = file_get_contents($this->storageFile);
+        return json_decode($data, true) ?? [];
     }
 
-    public function create(){
+    public function create() {}
 
-    }
+    public function update() {}
 
-    public function update(){
-
-    }
-
-    public function delete(){
-        
-    }
-
+    public function delete() {}
 }
--- view/Todo/home.php
+++ view/Todo/home.php
@@ -1,16 +1,31 @@
 <?php
-require_once __DIR__ .'/../../app/Controller/TaskManagerController.php';
+require_once __DIR__ . '/../../app/Controller/TaskManagerController.php';
 $taskManager = new TaskManagerController();
-
+$tasks = $taskManager->index();
 ?>
 <section class="w-full h-full">
-<div class="w-full bg-white px-6 py-4">
-    <h1 class="text-2xl font-bold text-blue-600 text-center">
-        My To-Do Task
-    </h1>
-</div>
-<div>
-    
-</div>
+    <div class="w-full bg-white px-6 py-4">
+        <h1 class="text-2xl font-bold text-blue-600 text-center">
+            My To-Do Task
+        </h1>
+    </div>
+    <div class="max-w-2xl mx-auto mt-6">
+        <ul class="space-y-3">
+
+            <?php foreach ($tasks as $task): ?>
+                <li class="flex items-center justify-between bg-white shadow-sm border rounded-lg px-4 py-3 hover:shadow-md transition">
+                    <span class="text-slate-700 font-medium">
+                        <?= $task['title'] ?>
+                    </span>
+
+                    <i class="fa-solid fa-circle-check 
+                    <?= ($task['status'] ?? '') === 'complete' ? 'text-green-500' : 'text-red-500' ?>">
+                    </i>
+
+                </li>
+            <?php endforeach; ?>
+
+        </ul>
+    </div>
 
 </section>
